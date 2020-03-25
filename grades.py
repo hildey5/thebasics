2771cdad6140569a4f57c37069defceff5ec00ad
Lucas Hilden <48299659+hildey5@users.noreply.github.com>
2020-03-25 10:51:08 -0300

    Add files via upload
    This program can calculate the average grade in a class.

---
+++ grades.py
@@ -0,0 +1,10 @@
+student_grades = [9.1, 8.8, 7.5]
+
+student_grades = {"Marry": 9.1, "Josh":8.8, "Mike": 7.5}
+
+mysum = sum(student_grades.values())
+length = len(student_grades)
+
+mean = mysum / length 
+
+print(mean)
