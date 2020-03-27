032c354a1ab9dbacdfb19ac16f92f224ee29d251
ahilden <ahilden@ca.ibm.com>
2020-03-27 02:42:17 -0400

    Program Improvements

--- grades.py
+++ grades.py
@@ -1,6 +1,6 @@
 student_grades = [9.1, 8.8, 7.5]
 
-student_grades = {"Marry": 9.1, "Josh":8.8, "Mike": 7.5}
+student_grades = {"Marry": 9.1, "Josh":8.8, "Mike": 7.5, "Lucas": 2.1}
 
 mysum = sum(student_grades.values())
 length = len(student_grades)
@@ -8,3 +8,9 @@ length = len(student_grades)
 mean = mysum / length 
 
 print(mean)
+
+for key, value in student_grades.items():
+    if value < 5.0:
+        print("Student", key, "needs to study more because he failed with a mark of", value)
+        if key == "Lucas":
+            print("But he has an awesome Dad!")
